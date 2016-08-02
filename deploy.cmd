e71615c0fecbdff7e4789c9c82707ef649ddeb9c
Gary Liu <v-guiliu@microsoft.com>
2016-08-02 12:18:59 +0800

    change directory name

--- deploy.cmd
+++ deploy.cmd
@@ -108,7 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. webpack
 IF EXIST "%DEPLOYMENT_TARGET%\webpack.config.js" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! webpack
+  call :ExecuteCmd !NPM_CMD! run webpack
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
